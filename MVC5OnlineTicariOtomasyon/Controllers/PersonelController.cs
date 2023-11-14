4de9e819d669e66fb67977688085e7b0af428df1
hazalzengin <hazal.helin.1907@hotmail.com>
2023-11-14 13:00:23 +0300

    Add project files.

---
+++ MVC5OnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5OnlineTicariOtomasyon.Models.Classes;
+namespace MVC5OnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View();
+        }
+    }
+}
